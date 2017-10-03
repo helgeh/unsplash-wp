a636f6c32f682ebf4fc049e444fc4d01a8b01e16
Helge Hofstad <helge@foxpublish.com>
2017-10-03 13:21:48 +0200

    use meow in stead of commander

--- cli.js
+++ cli.js
@@ -1,17 +1,21 @@
 #! /usr/bin/env node
-
 'use strict';
 
-const program = require('commander');
+const meow = require('meow');
 const chalk = require('chalk');
-const main = require('./main');
+const unsplashWp = require('.');
 
-program
-	.version('0.1.0')
-	.parse(process.argv);
+const cli = meow(`
+  Usage
+    $ unsplash-wp <folder>
 
-main().then(response => {
-	console.log(chalk.green('YEY'));
+  Examples
+    $ unsplash-wp "/home/unsplash-tmp/"
+    You now have a new wallpaper from Unsplash!
+`);
+
+unsplashWp({unsplashStore: cli.input[0]}).then(() => {
+  console.log(chalk.green('You now have a new wallpaper from Unsplash!'));
 }).catch(err => {
-	console.log(chalk.orange('Sorry, no go...', err));
+  console.log(chalk.orange('Sorry, no go...', err));
 });
